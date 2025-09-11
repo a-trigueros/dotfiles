4f30214aa9750f16159fcc6be126865c9b3c2a01
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2025-09-11 15:23:46 +0200

    update wezterm config for windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,35 +1,45 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local home = os.getenv("HOME")
-local nushell_config_dir = home .. "/.config/nushell/" 
-
--- Fonction pour lancer Nushell via un shell login (Zsh) afin d'hériter de l'environnement
-local function shell_command()
-  return { 
-	"/bin/zsh", 
-	"-l", 
-	"-c", 
-	'exec /opt/homebrew/bin/nu'
-  }
-end
-
-
-return {
-	-- Shell à utiliser
-	default_prog = shell_command(),
+local config = {
 	adjust_window_size_when_changing_font_size = false,
-	color_scheme = 'Catppuccin Mocha',
+	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font('JetBrains Mono'),
-	macos_window_background_blur = 30,
+	font = wezterm.font("JetBrains Mono"),
 	window_background_opacity = 0.8,
 	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
-	  {
-	    event = { Up = { streak = 1, button = 'Left' } },
-	    mods = 'CTRL',
-	    action = wezterm.action.OpenLinkAtMouseCursor,
-	  },
-	}
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
 }
+
+-- macOS spécifique
+if wezterm.target_triple:find("apple") then
+	-- Fonction pour lancer Nushell via un shell login (Zsh) afin d'hériter de l'environnement
+	local function shell_command()
+		return {
+			"/bin/zsh",
+			"-l",
+			"-c",
+			"exec /opt/homebrew/bin/nu",
+		}
+	end
+
+	config.default_prog = shell_command()
+	config.macos_window_background_blur = 30
+end
+
+-- Windows spécifique
+if wezterm.target_triple:find("windows") then
+	-- Mets à jour ce chemin si nécessaire
+	local nu_path = wezterm.home_dir .. "\\AppData\\Local\\Programs\\nu\\bin\\nu.exe"
+	config.default_prog = { nu_path }
+	config.prefer_egl = false
+end
+
+return config
+
