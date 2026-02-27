2911f6c3e06c71ad999a75c2782526af498e8b24
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2026-02-27 14:07:04 +0100

    fix: update config files after using mise

--- dot_config/zsh/node.zsh
+++ dot_config/zsh/node.zsh
@@ -7,6 +7,3 @@ export PATH="$PATH:$NPM_PACKAGES/bin"
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 
-source /opt/homebrew/opt/nvm/nvm.sh
-
-nvm use --lts --silent
