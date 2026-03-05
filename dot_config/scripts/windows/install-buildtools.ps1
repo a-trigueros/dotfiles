65f4823e74f774ca3a8c736153307e8e0d5b282e
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2026-03-05 11:28:26 +0100

    fix: Build Tools also include arm targets

--- dot_config/scripts/windows/install-buildtools.ps1
+++ dot_config/scripts/windows/install-buildtools.ps1
@@ -3,7 +3,9 @@ $vsInstaller = "$env:TEMP\vs_buildtools.exe"
 Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vs_buildtools.exe" -OutFile $vsInstaller
 
 # Run installer with arguments
- Start-Process -FilePath $vsInstaller -ArgumentList `
-    "--quiet --wait --norestart --nocache `
+Start-Process -FilePath $vsInstaller -ArgumentList `
+  "--quiet --wait --norestart --nocache `
     --add Microsoft.VisualStudio.Workload.VCTools `
+    --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 `
+    --add Microsoft.VisualStudio.Component.VC.Tools.ARM64EC `
     --includeRecommended" -Wait
