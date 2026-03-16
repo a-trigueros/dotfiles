e54abb7ce8818daf0d79bfae877b39ab997cba51
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2026-03-16 22:46:54 +0100

    feat: basic powershell & pwsh tools configuration

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,9 @@
 $env:path += ";$env:LOCALAPPDATA\mise\shims"
+
+$env:CARAPACE_BRIDGES = 'pwsh,zsh,fish,bash,inshellisense' # optional
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
+starship init powershell | Out-String | Invoke-Expression
+zoxide init powershell | Out-String | Invoke-Expression
+
+atuin init powershell | Out-String | Invoke-Expression
